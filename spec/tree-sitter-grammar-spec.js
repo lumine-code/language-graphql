a9cc52bb8490cc2b387a0cc4be6028c665ab0b37
Sebastian Balcerowiak <asiloisad.93@gmail.com>
2026-08-09 18:54:17 +0200

    Complete the Lumine ecosystem rebrand

--- spec/tree-sitter-grammar-spec.js
+++ spec/tree-sitter-grammar-spec.js
@@ -9,7 +9,7 @@ const path = require("path");
 
 describe("GraphQL Tree-sitter grammar", () => {
   beforeEach(async () => {
-    await atom.packages.activatePackage("language-graphql");
+    await lumine.packages.activatePackage("language-graphql");
   });
 
   it("tokenizes the fixture", async () => {
